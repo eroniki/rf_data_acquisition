c547fdbc82fcbb7a18470257069afad2ed909925
murat ambarkutuk <m.ambarkutuk@gmail.com>
2017-03-20 12:39:01 -0400

    Update the loss function

--- etc/db/path_loss.m
+++ etc/db/path_loss.m
@@ -1,8 +1,8 @@
 close all;
-verbose = 0;
 %% Function Handles %
-loss2 = @(d, d_hat, n, n_test_max) (d-d_hat).^2 + n/n_test_max;
-loss = @(d, d_hat, n, n_test_max) (d-d_hat).^2;
+loss_regularization = @(n)(n)^1.2;
+loss_localization = @(d, d_hat) (d-d_hat).^2;
+loss_total = @(l1, l2, w) w*l1 + (1-w)*l2;
 localization_error = @(d, d_hat) sqrt(sum((d - d_hat).^2,2));
 z_score = @(x) (x-mean(x,'omitnan')) ./ std(x, 'omitnan');
 circle = @(x,y,r,ang) deal(x+r*cos(ang), y+r*sin(ang));
@@ -19,7 +19,7 @@ dist = pdist2(pos_node,center);
 
 pl = pt - lora_propagation;
 pl_at_center = pt - pr_at_center;
-%% Collective Path Loss Exponent
+%% Collective Path Loss Exponent Estimation
 n_test = 1:0.02:8;
 e_bar = [];
 n_tested = [];
@@ -33,18 +33,21 @@ for n=n_test
             pl_vector = pl_vector(:);
             d_hat = ldpl(dist, pl_vector, pl_at_center, n, std_lora);
             d = pdist2(pos_node, test_point);
-            e =  loss(d_hat,d, n, max(n_test));
-            loss_map(i,j,:) = e;
+            ll = loss_localization(d,d_hat);
+            loss_map(i,j,:) = ll;
         end
     end
-    buff = mean(loss_map(:), 'omitnan');
+    rl = loss_regularization(n);
+    ll = mean(loss_map(:), 'omitnan');
+    buff = loss_total(ll, rl, 0.9);
     e_bar = [e_bar, buff];
     if(verbose)
-        disp(['n: ', num2str(n), ' e_bar: ', num2str(buff), num2str(size(e_bar))]);
+        disp(['n: ', num2str(n), ' ll: ', num2str(ll), ' rl:', num2str(rl), ' sum: ', num2str(buff)]);
     end
     figure(1);
     plot(n_tested, e_bar); grid on; grid minor;
     xlim([min(n_test), max(n_test)]);
+    ylim([0, 1000]);
     xlabel('Path Loss Exponent (n)');
     ylabel('Loss');
     drawnow;
@@ -68,21 +71,25 @@ for n=n_test
             pl_vector = pl_vector(:);
             d_hat = ldpl(dist, pl_vector, pl_at_center, n, std_lora);
             d = pdist2(pos_node, test_point);
-            e =  [e, loss(d_hat,d, n, max(n_test))];
+            ll = loss_localization(d,d_hat);
+            e =  [e, ll];
 %             loss_map(i,j,:) = e;
         end
     end
-    buff = mean(e, 2, 'omitnan');
+    ll = mean(e, 2, 'omitnan');
+    rl = loss_regularization(n);
+    buff = loss_total(ll, rl, 0.9);
     e_bar = [e_bar, buff];
     if(verbose)
         disp(['n: ', num2str(n), ' e_bar: ', num2str(buff'), num2str(size(e_bar))]);
     end
-    figure(1);
+    figure(2);
     plot(n_tested, e_bar(1,:), n_tested, e_bar(2,:), n_tested, e_bar(3,:), ...
         n_tested, e_bar(4, :), n_tested, e_bar(5, :), n_tested, e_bar(6,:), ...
         n_tested, e_bar(7, :), n_tested, e_bar(8, :)); grid on; grid minor;
     legend('0', '1', '2', '3', '4', '5', '6', '7');
     xlim([min(n_test), max(n_test)]);
+    ylim([0, 1000]);
     xlabel('Path Loss Exponent (n)');
     ylabel('Loss');
     drawnow;
@@ -93,7 +100,6 @@ disp('Path Loss Exponent:');
 disp(vpa(path_loss_exp_ind, 8));
 %% Re-evaluate the localization performance with the optimal ple
 %% Grid-Localization with LDPL
-close all;
 error_map = zeros(25,7,8);
 error_map_ind = zeros(25,7,8);
 for i=1:25
@@ -112,7 +118,6 @@ for i=1:25
     end
 end
 %% Least-squares Sense Localization
-close all;
 e_vec = [];
 e_vec_ind = [];
 for i=1:25
@@ -122,13 +127,13 @@ for i=1:25
     pl_vector = pl_vector(:);
     d_hat = ldpl(dist, pl_vector, pl_at_center, path_loss_exp, std_lora);
     d_hat_ind = ldpl(dist, pl_vector, pl_at_center, path_loss_exp_ind, std_lora);
-    for k=1:8
-        [x,y] = circle(pos_node(k,1), pos_node(k,2), d_hat(k), 0:0.001:2*pi);
-        figure(1); plot(x,y); grid on; hold on;
-        xlim([0,8]);
-        ylim([0,25]);
-        view([0, -90]);
-    end
+%     for k=1:8
+%         [x,y] = circle(pos_node(k,1), pos_node(k,2), d_hat(k), 0:0.001:2*pi);
+%         figure(1); plot(x,y); grid on; hold on;
+%         xlim([0,8]);
+%         ylim([0,25]);
+%         view([0, -90]);
+%     end
     % first anchor node is linearization pivot
     A = [pos_node(2:8,1) - pos_node(1,1), pos_node(2:8,2) - pos_node(1,2)];  
     d_sq = d_hat(2:8).^2;
@@ -138,8 +143,8 @@ for i=1:25
     b_ind = (d_hat_ind(1)^2 - d_sq_ind + distance_between_an)/2;
     x = (inv(A'*A)*A'*b)';
     x_ind = (inv(A'*A)*A'*b_ind)';
-    plot(test_point(2), test_point(1), 'bd', x(1), x(2), 'r*'); hold off;
-    view([0, -90]);
+%     plot(test_point(2), test_point(1), 'bd', x(1), x(2), 'r*'); hold off;
+%     view([0, -90]);
 %     pause;
     test_point = [test_point(2), test_point(1)];
     e = test_point - x;
@@ -150,5 +155,7 @@ for i=1:25
     e_vec_ind = [e_vec_ind, e_ind];
     end
 end
-mean(e_vec, 'omitnan')
-mean(e_vec_ind, 'omitnan')
+disp('Mean localization error (w/ Joint Path Loss Estimation):');
+disp(mean(e_vec, 'omitnan'));
+disp('Mean localization error (w/ Individual Path Loss Estimation):');
+disp(mean(e_vec_ind, 'omitnan'));
