6b526bc3c7e576f1ac928e888c28a60197f7ad04
murat ambarkutuk <m.ambarkutuk@gmail.com>
2017-03-23 16:40:09 -0400

    Move non-db related stuff into wifi_regression submodule
    I believe this is more logical way of maintaining the repositories.

--- etc/db/find_closest.m
+++
@@ -1,4 +0,0 @@
-function [val, idx] = find_closest(f,x)
-    [val, idx] = min(abs(f-x));
-end
-
--- etc/db/ldpl.m
+++
@@ -1,5 +0,0 @@
-function [d_hat] = ldpl(d0, pl_d, pl_d0, n, std)
-    rnd_= normrnd(zeros([numel(pl_d), 1]), std');
-    d_hat = 10.^((pl_d - pl_d0 - rnd_)./(10*n)).*d0;
-end
-
